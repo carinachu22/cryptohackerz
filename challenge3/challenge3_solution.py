94f012b8b4c74443d3587d15bb0022ff91fa33be
akashcx <akashchandrasekar@gmail.com>
2023-08-03 20:22:06 +0800

    Challenge 3 Code

---
+++ challenge3/challenge3_solution.py
@@ -0,0 +1,59 @@
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def decrypt(ciphertext, d, n):
+    text = [chr(pow(i, d, n)) for i in ciphertext]
+    return "".join(text)
+
+def solve(encrypted_message, encrypted_n, encrypted_e):
+    mapping = {
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "D": 4,
+        "E": 5,
+        "F": 6,
+        "G": 7,
+        "H": 8,
+        "I": 9,
+        "X": 0
+    }
+
+    encrypted = encrypted_message.split()
+    message = []
+    for i in encrypted:
+        x = [str(mapping[j]) for j in i]
+        message.append(int("".join(x)))
+
+    n = int("".join([str(mapping[j]) for j in encrypted_n]))
+    e = int("".join([str(mapping[j]) for j in encrypted_e]))
+
+    factors = prime_factors(n)
+    p, q = factors[0], factors[1]
+    z = (p - 1) * (q - 1)
+
+    d = 0
+    while True:
+        if (e * d) % z == 1:
+            break
+        d += 1
+
+    flag = decrypt(message, d, n)
+    return flag
+
+encrypted_message = "AABI BAFI BE ACHC IFH CEBG CIBA CI AHAX HGE BHED IAD AHAX AFHA AFBF AXCE DGB CIBA AXCE CAFD ADA"
+encrypted_n = "DAHG"
+encrypted_e = "ABCG"
+
+rsa = solve(encrypted_message, encrypted_n, encrypted_e)
+print(rsa)
---
+++ challenge3/challenge3_students.py
@@ -0,0 +1,8 @@
+## Task: To find the private key d given the public key (n, e). The private key d is used to decrypt the Encrypted Message to get the flag.
+
+## Hint: Encrypted Message needs to be converted to a number first. The number is then decrypted using the private key d and n.
+
+
+encrypted_message = "AABI BAFI BE ACHC IFH CEBG CIBA CI AHAX HGE BHED IAD AHAX AFHA AFBF AXCE DGB CIBA AXCE CAFD ADA"
+encrypted_n = "DAHG"
+encrypted_e = "ABCG"
