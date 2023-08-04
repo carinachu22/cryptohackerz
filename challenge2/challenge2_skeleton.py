cdfa32ed758ef37484a32874269a322b6aed158d
Muthuvel <usvimal@gmail.com>
2023-08-04 21:21:53 +0800

    Remove students

--- challenge2/challenge2_skeleton.py
+++ challenge2/challenge2_skeleton.py
@@ -1,11 +1,19 @@
+
+## Task: Decrypt the ciphertext using the given information
+
+"""
+Hint: Figure out what kind of cipher it is and find the key for the cipher. The solution for challenge 1 can be used to find the key.
+"""
+
 def decode_cipher(ciphertext, key):
-    # TODO: Figure out the type of cipher and write out the implementation for decoding it
+    # TODO 3: write out the implementation for decoding it
     plaintext = ""
     return(plaintext)
 
 if __name__ == "__main__":
     ciphertext = "NCQXMIF GBVT HYDBQFBG DCQT"
-
-    # TODO: Find the key
+    # TODO 1: Figure out the type of cipher
+    cipher = "V"
+    # TODO 2: Find the key
     key = ""
     print(decode_cipher(ciphertext, key))
