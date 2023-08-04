e8c1953a920be9bc452527f1170317ae5639a35f
akashcx <akashchandrasekar@gmail.com>
2023-08-04 20:42:37 +0800

    Updated Challenge 3

--- challenge3/challenge3_skeleton.py
+++ challenge3/challenge3_skeleton.py
@@ -1,3 +1,10 @@
+## Task: To find the private key d given the public key (n, e). The private key d is used to decrypt the Encrypted Message to get the flag.
+
+"""
+Hint: Encrypted Message needs to be converted to a number first. The number is then decrypted using the private key d and n. 
+Anya can only count to 3 digits.
+"""
+
 def prime_factors(n):
     # TODO: Modify to return the prime factors of a number n
     factors = []
